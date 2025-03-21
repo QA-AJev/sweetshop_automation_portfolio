b611f89e30a3fc43286253c05639c4d4f156e395
QA-AJev <artjev28@gmail.com>
2025-03-21 12:52:11 +0200

    Tests reworked 1.03

--- cypress/e2e/01test.cy.js
+++ cypress/e2e/01test.cy.js
@@ -35,5 +35,26 @@ describe('Basic Functionality Check - Navigation & Links', () => {
         cy.get('a.nav-link[href="/login"]').click();
         cy.url().should('include', '/login');
         cy.contains('Login').should('be.visible');
+
+
+    });
+    
+    it('Verifies nav bar links from the basket page remain functional', () => {
+        
+        cy.get('a.nav-link[href="/basket"]').click();
+        cy.url().should('include', '/basket');
+        cy.contains('Basket').should('be.visible');
+
+        // Check the nav bar links
+        cy.get('a.nav-link').each(($link) => {
+            const linkHref = $link.attr('href');
+            
+            cy.wrap($link).should('have.attr', 'href').and('match', /^\/(sweets|about|login|basket)$/);  // Checks the possible links
+        });
+
+        cy.get('a.nav-link[href="/sweets"]').should('have.attr', 'href', '/sweets');
+        cy.get('a.nav-link[href="/about"]').should('have.attr', 'href', '/about');
+        cy.get('a.nav-link[href="/login"]').should('have.attr', 'href', '/login');
+        cy.get('a.nav-link[href="/basket"]').should('have.attr', 'href', '/basket');
     });
 });
